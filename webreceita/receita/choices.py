71fbfe56b162537de924cff7dcab050bd6f40d4d
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2015-10-06 22:38:57 -0300

    Adds models and choices file. WIP

---
+++ webreceita/receita/choices.py
@@ -0,0 +1,11 @@
+"""
+Arquivo para definir as escolhas possiveis para um Model da receita
+"""
+
+COZIMENTO_ESCOLHAS = (
+    (1, _("Frito")),
+    (2, _("Cozido")),
+    (3, _("Assado")),
+    (4, _("Refogado")),
+    (5, _("Grelhar")),
+)
--- webreceita/receita/models.py
+++ webreceita/receita/models.py
@@ -1,3 +1,40 @@
-# from django.db import models
+from django.db import models
+from choices import *
+from django.contrib.localflavor.br.br_states import STATE_CHOICES
 
-# Create your models here.
+
+class Receita(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=300)
+    autor = models.ForeignKey(Autor)
+    categoria = models.ForeignKey(Categoria)
+    instrucao = models.TextField(max_length=800)
+    porcoes = models.IntegerField()
+    valor_nutricional = models.IntegerField()
+    metodo_cozimento = models.IntegerField(choices=COZIMENTO_ESCOLHAS, default=1)
+
+
+class ReceitaImagem(models.Model):
+    image = models.ImageField()
+    ref = models.ForeignKey(Receita)
+
+
+class Autor(models.Model):
+    nome_completo = models.CharField(max_length=200)
+    nascimento = models.DateTimeField()
+    cidade = models.CharField(max_length=200)
+    estado = models.IntegerField(choices=STATE_CHOICES)
+    telefone = models.CharField(max_length=15)
+    login = models.EmailField(max_length=200)
+    senha = models.CharField(max_length=30)
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+
+
+class Ingredientes(models.Model):
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    unidade = models.CharField(max_length=20)
+    receita = models.ForeignKey(Receita)
