53d326c1dd45a7382e702e99cdd262a2cfec81d9
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2015-10-08 21:57:21 -0300

    mend

--- webreceita/receita/models.py
+++ webreceita/receita/models.py
@@ -3,6 +3,20 @@ from choices import COZIMENTO_ESCOLHAS
 from django.contrib.localflavor.br.br_states import STATE_CHOICES
 
 
+class Autor(models.Model):
+    nome_completo = models.CharField(max_length=200)
+    nascimento = models.DateTimeField()
+    cidade = models.CharField(max_length=200)
+    estado = models.IntegerField(choices=STATE_CHOICES)
+    telefone = models.CharField(max_length=15)
+    login = models.EmailField(max_length=200)
+    senha = models.CharField(max_length=30)
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+
+
 class Receita(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.TextField(max_length=300)
@@ -17,20 +31,6 @@ class Receita(models.Model):
 class ReceitaImagem(models.Model):
     image = models.ImageField()
     ref = models.ForeignKey(Receita)
-
-
-class Autor(models.Model):
-    nome_completo = models.CharField(max_length=200)
-    nascimento = models.DateTimeField()
-    cidade = models.CharField(max_length=200)
-    estado = models.IntegerField(choices=STATE_CHOICES)
-    telefone = models.CharField(max_length=15)
-    login = models.EmailField(max_length=200)
-    senha = models.CharField(max_length=30)
-
-
-class Categoria(models.Model):
-    nome = models.CharField(max_length=200)
 
 
 class Ingredientes(models.Model):
