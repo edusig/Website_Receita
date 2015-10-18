d42dc9f8ccefa659e7d84805a7a47b0058e2728d
Hiero Martinelli <hieromartinelli@gmail.com>
2015-10-18 03:46:10 -0200

    Esqueleto da página inicial criado, {% block conteudo %} ainda não funciona
    O conteúdo da página ainda não é carregado dinamicamente pelo Django.
    Algumas mudanças feitas em base.html. Sidebar ainda não possui nada.

--- webreceita/receita/views.py
+++ webreceita/receita/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
-
+from django.template import RequestContext
+from receita.models import *
 
 # Create your views here.
 def index(req):
-    return render_to_response('index.html')
+    categorias = Categoria.objects.all()[:3]
+    context = RequestContext(req, {'categorias':categorias})
+    return render_to_response('index.html', context)
