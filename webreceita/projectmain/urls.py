d0550b3745f1b5d9febc34829a72ded90f972ad3
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2015-10-27 19:23:30 -0200

    Login and Authentication initial commit

--- webreceita/projectmain/urls.py
+++ webreceita/projectmain/urls.py
@@ -7,10 +7,10 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'receita.views.index', name='index'),
-    url(r'^login$', 'receita.views.login', name='login'),
-    url(r'^register', 'receita.views.register', name='cadastro'),
-    url(r'^categoria/(?P<cat>[a-zA-Z0-9 -_]+)', 'receita.views.categoria', name='categoria'),
-    url(r'^cadastro_receita', 'receita.views.cadastro_receita', name='cadastro_receita'),
-    url(r'^detalhe_receita/([a-zA-Z0-9 -_]+)', 'receita.views.detalhe_receita', name='detalhe_receita'),
+    url(r'^login/$', 'receita.views.login', name='login'),
+    url(r'^register/', 'receita.views.register', name='cadastro'),
+    url(r'^categoria/(?P<cat>[a-zA-Z0-9 -_]+)/', 'receita.views.categoria', name='categoria'),
+    url(r'^cadastro_receita/', 'receita.views.cadastro_receita', name='cadastro_receita'),
+    url(r'^detalhe_receita/([a-zA-Z0-9 -_]+)/', 'receita.views.detalhe_receita', name='detalhe_receita'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webreceita/receita/views.py
+++ webreceita/receita/views.py
@@ -1,53 +1,73 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from receita.models import *
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(req):
-    categorias = Categoria.objects.all()
+    cdict = load_menu_sidebar()
     receitas = []
-    for cat in categorias:
+    for cat in cdict['categorias']:
         receitas += [{'Nome': cat.nome, 'categoria': True}]
         receitas = receitas + [{'categoria': False, 'receita': x} for x in Receita.objects.filter(categoria=cat)[:3]]
-    context = RequestContext(req, {'categorias': categorias, 'receitas': receitas, 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()})
-    print(receitas)
+    cdict.update({'receitas': receitas})
+    context = RequestContext(req, cdict)
     return render_to_response('index.html', context)
 
 
 def login(req):
-    categorias = Categoria.objects.all()
-    context = RequestContext(req, {'categorias':categorias, 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()})
+    cdict = load_menu_sidebar()
+    loginresult = None
+    if req.method == 'POST':
+        username = req.POST['username']
+        password = req.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(req, user, 'index.html')
+            else:
+                loginresult = 'disabled'
+        else:
+            loginresult = 'invalid'
+    cdict.update({'result': loginresult})
+    context = RequestContext(req, cdict)
     return render_to_response('login.html', context)
 
 
 def register(req):
-    categorias = Categoria.objects.all()
-    context = RequestContext(req, {'categorias':categorias, 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()})
+    cdict = load_menu_sidebar()
+    context = RequestContext(req, cdict)
     return render_to_response('cadastro.html', context)
 
 
+def user_logout(req):
+    logout(req, 'index.html')
+
+
 def categoria(req, cat):
-    categorias = Categoria.objects.all()
+    cdict = load_menu_sidebar()
     categoria_select = Categoria.objects.get(nome=cat)
     receitas = Receita.objects.filter(categoria=categoria_select)
-    context = RequestContext(req, {'categorias': categorias, 'cat': cat, 'receitas': receitas, 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()})
+    cdict.update({'cat': cat, 'receitas': receitas})
+    context = RequestContext(req, cdict)
     return render_to_response('receita_categoria.html', context)
 
 
 def cadastro_receita(req):
-    categorias = Categoria.objects.all()
-    context = RequestContext(req, {'categorias':categorias, 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()})
+    cdict = load_menu_sidebar()
+    context = RequestContext(req, cdict)
     return render_to_response('cadastro_receita.html', context)
 
 
 def detalhe_receita(req, pk):
-    categorias = Categoria.objects.all()
+    cdict = load_menu_sidebar()
     receita = Receita.objects.get(pk=pk)
     ingredientes = Ingredientes.objects.filter(receita=receita)
     receita.instrucao = receita.instrucao.split('\n')
     receita.metodo_cozimento = COZIMENTO_ESCOLHAS[receita.metodo_cozimento][1]
     bebida = receita.categoria.nome == 'Bebidas'
-    context = RequestContext(req, {'categorias': categorias, 'receita': receita, 'ingredientes': ingredientes, 'isbebida': bebida, 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()})
+    cdict.update({'receita': receita, 'ingredientes': ingredientes, 'isbebida': bebida})
+    context = RequestContext(req, cdict)
     return render_to_response('receita.html', context)
 
 
@@ -60,3 +80,6 @@ def top_receitas():
     pks = [3,5,6]
     return Receita.objects.filter(pk__in=pks)
 
+
+def load_menu_sidebar():
+    return {'categorias' : Categoria.objects.all(), 'receita_do_dia': receita_do_dia(), 'top_receitas': top_receitas()}
